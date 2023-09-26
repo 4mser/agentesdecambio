e5b70e9cce3be171a59266597f946ea3184eff30
4mser <nicolas.moreno.sagan@gmail.com>
2023-09-26 02:17:38 -0300

    colores adaptados

--- src/components/AgentesDeCambio.jsx
+++ src/components/AgentesDeCambio.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import styled from "styled-components";
+import styled, { keyframes, css } from "styled-components";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -11,6 +11,7 @@ import "swiper/css/pagination";
 import { Mousewheel, Pagination } from "swiper/modules";
 
 const AgentesDeCambio = () => {
+  const [isFirstSlider, setIsFirstSlider] = useState(true);
   const [menuOpen, setMenuOpen] = useState(false);
 
   const toggleMenu = () => {
@@ -89,89 +90,98 @@ const AgentesDeCambio = () => {
         }}
         modules={[Mousewheel, Pagination]}
         className="mySwiper"
+        onSlideChange={(swiper) => {
+          setIsFirstSlider(swiper.activeIndex === 0);
+        }}
       >
         <SwiperSlide>
-          <LandImages>
-            <img
-              style={
-                innerWidth > 1000
-                  ? {
-                      transform: `translate(${-limitedX}%, ${limitedY}%)`,
-                      transition: "transform .5s ease-out",
-                    }
-                  : {
-                      transform: `translate(${-limitedX}%, ${limitedY}%)`,
-                      transition: "transform .5s ease-out",
-                    }
-              }
-              src="https://appentropia.s3.amazonaws.com/agentesCambio/1.png"
-              alt=""
-            />
-            <img
-              style={
-                innerWidth > 1000
-                  ? {
-                      transform: `translate(${limitedX}%, ${-limitedY}%)`,
-                      transition: "transform .1s ease-out",
-                    }
-                  : {
-                      transform: `translate(${limitedX}%, ${-limitedY}%)`,
-                      transition: "transform .1s ease-out",
-                    }
-              }
-              src="https://appentropia.s3.amazonaws.com/agentesCambio/2.png"
-              alt=""
-            />
-            <img
-              style={
-                innerWidth > 1000
-                  ? {
-                      transform: `translate($-limitedX}%, ${limitedY}%)`,
-                      transition: "transform .5s ease-out",
-                    }
-                  : {
-                      transform: `translate(${limitedX}%, ${limitedY}%)`,
-                      transition: "transform .5s ease-out",
-                    }
-              }
-              src="https://appentropia.s3.amazonaws.com/agentesCambio/3.png"
-              alt=""
-            />
-            <img
-              style={
-                innerWidth > 1000
-                  ? {
-                      transform: `translate(${limitedX}%, ${limitedY}%)`,
-                      transition: "transform .1s ease-out",
-                    }
-                  : {
-                      transform: `translate(${limitedX}%, ${limitedY}%)`,
-                      transition: "transform .1s ease-out",
-                    }
-              }
-              src="https://appentropia.s3.amazonaws.com/agentesCambio/4.png"
-              alt=""
-            />
-            <img
-              style={
-                innerWidth > 1000
-                  ? {
-                      transform: `translate(${-limitedX}%, ${-limitedY}%)`,
-                      transition: "transform .1s ease-out",
-                    }
-                  : {
-                      transform: `translate(${-limitedX}%, ${-limitedY}%)`,
-                      transition: "transform .1s ease-out",
-                    }
-              }
-              src="https://appentropia.s3.amazonaws.com/agentesCambio/personas.png"
-              alt=""
-            />
-          </LandImages>
-          <Titulo>
-            <h2>AGENTES DE CAMBIO</h2>
-            <p>Hacia una Organización Sostenible</p>
-          </Titulo>
+          <LandPage>
+            <LandImages isFirstSlider={isFirstSlider}>
+              <img
+                style={
+                  innerWidth > 1000
+                    ? {
+                        transform: `translate(${-limitedX}%, ${limitedY}%)`,
+                        transition: "transform .5s ease-out",
+                      }
+                    : {
+                        transform: `translate(${-limitedX}%, ${limitedY}%)`,
+                        transition: "transform .5s ease-out",
+                      }
+                }
+                src="https://appentropia.s3.amazonaws.com/agentesCambio/1.png"
+                alt=""
+              />
+              <img
+                style={
+                  innerWidth > 1000
+                    ? {
+                        transform: `translate(${limitedX}%, ${-limitedY}%)`,
+                        transition: "transform .1s ease-out",
+                      }
+                    : {
+                        transform: `translate(${limitedX}%, ${-limitedY}%)`,
+                        transition: "transform .1s ease-out",
+                      }
+                }
+                src="https://appentropia.s3.amazonaws.com/agentesCambio/2.png"
+                alt=""
+              />
+              <img
+                style={
+                  innerWidth > 1000
+                    ? {
+                        transform: `translate($-limitedX}%, ${limitedY}%)`,
+                        transition: "transform .5s ease-out",
+                      }
+                    : {
+                        transform: `translate(${limitedX}%, ${limitedY}%)`,
+                        transition: "transform .5s ease-out",
+                      }
+                }
+                src="https://appentropia.s3.amazonaws.com/agentesCambio/3.png"
+                alt=""
+              />
+              <img
+                style={
+                  innerWidth > 1000
+                    ? {
+                        transform: `translate(${limitedX}%, ${limitedY}%)`,
+                        transition: "transform .1s ease-out",
+                      }
+                    : {
+                        transform: `translate(${limitedX}%, ${limitedY}%)`,
+                        transition: "transform .1s ease-out",
+                      }
+                }
+                src="https://appentropia.s3.amazonaws.com/agentesCambio/4.png"
+                alt=""
+              />
+              <img
+                style={
+                  innerWidth > 1000
+                    ? {
+                        transform: `translate(${-limitedX}%, ${-limitedY}%)`,
+                        transition: "transform .1s ease-out",
+                      }
+                    : {
+                        transform: `translate(${-limitedX}%, ${-limitedY}%)`,
+                        transition: "transform .1s ease-out",
+                      }
+                }
+                src="https://appentropia.s3.amazonaws.com/agentesCambio/personas.png"
+                alt=""
+              />
+            </LandImages>
+            <Titulo isFirstSlider={isFirstSlider}>
+              <h2>AGENTES DE CAMBIO</h2>
+              <p>Hacia una organización sostenible</p>
+            </Titulo>
+            <Desliza>
+              DESLIZA
+              <div className="desliza"></div>
+            </Desliza>
+          </LandPage>
         </SwiperSlide>
         <SwiperSlide>
           <p>
@@ -212,16 +222,33 @@ export default AgentesDeCambio;
 const Container = styled.div`
   width: 100%;
   height: 100vh;
-  background-image: radial-gradient(
-    circle at 85.36% -11.24%,
-    #f6fca9 0,
-    #e1f5a0 16.67%,
-    #c6e992 33.33%,
-    #a4d880 50%,
-    #80c770 66.67%,
-    #5ebb69 83.33%,
-    #3ab368 100%
-  );
+  /* background-image: radial-gradient(
+    circle at 0% 0%,
+    #ffe7d1 0,
+    #ffe7ce 4.55%,
+    #ffe8ca 9.09%,
+    #fee7c6 13.64%,
+    #f7e6c1 18.18%,
+    #efe5bc 22.73%,
+    #e5e3b7 27.27%,
+    #dae0b1 31.82%,
+    #cdddac 36.36%,
+    #bed9a7 40.91%,
+    #add5a3 45.45%,
+    #99d09f 50%,
+    #84cb9d 54.55%,
+    #6dc69e 59.09%,
+    #53c2a1 63.64%,
+    #32bea6 68.18%,
+    #00bbac 72.73%,
+    #00b7b4 77.27%,
+    #00b4bd 81.82%,
+    #00b1c5 86.36%,
+    #00afce 90.91%,
+    #00add6 95.45%,
+    #00abde 100%
+  ); */
+  background: #e9f5ee;
   position: fixed;
   overflow: hidden;
   z-index: 9999;
@@ -231,7 +258,6 @@ const Container = styled.div`
   .mySwiper {
     width: 100%;
     height: 100vh;
-    padding: 2rem;
   }
 
   .swiper-slide {
@@ -250,7 +276,7 @@ const Container = styled.div`
   }
 
   .swiper-pagination-bullet {
-    background: white;
+    background: var(--green);
     height: 8px;
     width: 8px;
     border-radius: 50%;
@@ -304,17 +330,17 @@ const Header = styled.div`
   }
 
   .botonNavegador .nav2 .btn1 {
-    width: 50px;
+    width: 40px;
     height: 3px;
-    background: white;
+    background: var(--green);
     border-radius: 5px;
     transition: all 0.3s ease-in-out;
   }
 
   .botonNavegador .nav2 .btn2 {
-    width: 50px;
+    width: 40px;
     height: 3px;
-    background: white;
+    background: var(--green);
     border-radius: 5px;
     transition: all 0.3s ease-in-out;
   }
@@ -330,9 +356,41 @@ const Header = styled.div`
   }
 `;
 
+const LandPage = styled.div`
+  width: 100%;
+  height: 80vh;
+  top: 0;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const TitleAnimation = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(-40px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+
+const ImagesAnimation = keyframes`
+  from {
+    transform: translateX(40px);
+    opacity: 0;
+  }
+  to {
+    transform:  translateX(0);
+    opacity: 1;
+  }
+`;
+
+// Estilos condicionales para LandImages y Titulo
 const LandImages = styled.div`
-  width: 70vw;
-  height: 70vw;
+  height: 40vh;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -344,23 +402,59 @@ const LandImages = styled.div`
     object-fit: contain;
     position: absolute;
   }
+
+  ${(props) =>
+    props.isFirstSlider &&
+    css`
+      animation: ${ImagesAnimation} 0.8s ease-in-out;
+    `}
 `;
 
 const Titulo = styled.div`
   width: 100%;
-  height: 3rem;
   display: flex;
   justify-content: space-between;
   flex-direction: column;
-
+  padding: 15px 1rem;
   h2 {
-    font-size: 10vw;
-    margin: 0; /* Eliminamos el margen predeterminado del h2 */
+    font-size: 12vw;
+    margin: 0;
+    color: var(--green);
   }
 
   p {
-    font-size: 4vw;
-    opacity: 0.9;
-    margin: 0; /* Eliminamos el margen predeterminado del p */
+    font-size: 4.5vw;
+    font-weight: 500;
+    margin: 0;
+    color: var(--color2);
+  }
+
+  ${(props) =>
+    props.isFirstSlider &&
+    css`
+      animation: ${TitleAnimation} 0.8s ease-in-out;
+    `}
+`;
+
+const Desliza = styled.div`
+  position: fixed;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  width: 1rem;
+  height: 4.7rem;
+  font-weight: 500;
+  bottom: 0;
+  right: 1rem;
+  writing-mode: vertical-lr;
+  font-size: 8px;
+  color: var(--color2);
+
+  .desliza {
+    width: 1px;
+    opacity: 0.5;
+    height: 2rem;
+    background: var(--color2);
   }
 `;
